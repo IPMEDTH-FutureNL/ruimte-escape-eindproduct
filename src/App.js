47c3cb942332467c264587f8c16840b4d324c424
daniquemont <danique.montanus@gmail.com>
2022-05-30 13:18:28 +0200

    intro scherm

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import './App.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
-import IntroPuzzle from './components/startPuzzle/IntroPuzzle';
-import OvergangPuzzel from './components/startPuzzle/OvergangPuzzel';
-import DragAndDrop from './components/startPuzzle/DragAndDrop';
-import IntroMatrix from './components/secondPuzzle/IntroMatrix';
-import Oplosmatrix from './components/secondPuzzle/Oplosmatrix';
+import Start from './components/verhaal/Start';
+import IntroPuzzle from './components/dragAndDrop/IntroPuzzle';
+import OvergangPuzzel from './components/dragAndDrop/OvergangPuzzel';
+import DragAndDrop from './components/dragAndDrop/DragAndDrop';
+import IntroMatrix from './components/oplosMatrix/IntroMatrix';
+import Oplosmatrix from './components/oplosMatrix/Oplosmatrix';
 import QuizzPuzzle from './components/quizzPuzzle/QuizzPuzzle';
 import RebusPuzzle from './components/rebus/RebusPuzzle';
 
@@ -14,7 +15,8 @@ function App() {
     <Router>
       <div className="App">
         <Routes>
-            <Route path="/" element={ <IntroPuzzle /> }></Route>
+            <Route path="/" element={ <Start /> }></Route>
+            <Route path="/introPuzzle" element={ <IntroPuzzle /> }></Route>
             <Route path="/introPuzzelEen" element={<OvergangPuzzel/>}></Route>
             <Route path="/puzzelEen" element={ <DragAndDrop />}></Route>
             <Route path="/introMatrix" element={ <IntroMatrix /> }></Route>
