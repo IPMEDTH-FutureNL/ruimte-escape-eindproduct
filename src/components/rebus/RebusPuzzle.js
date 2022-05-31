0a431a1014c7be0792bc6d234b632064807771c2
Burak Kivrak <m-burak09@hotmail.com>
2022-05-31 15:53:23 +0200

    rebus intro

--- src/components/rebus/RebusPuzzle.js
+++ src/components/rebus/RebusPuzzle.js
@@ -20,11 +20,19 @@ const RebusPuzzle = () => {
         //play locked sound
     }
 
+    const exit = () => {
+        document.getElementById('open').id = "exit";
+    }
+
   return (
     <section className="background">
         <img src={RebusBackground} alt="" className="rebus-background" />
         <section className='block-rebus' onClick={setModalIsOpenToTrue}></section>
         <section className='handle-rebus' onClick={locked}></section>
+        <div className="quiz-intro" id="open">
+            <p className="text-white">Goedzo, Jullie hebben de code gekraakt! Ga nu naar de cockpit om het vuilnis op te kunnen ruimen! Samen met alle andere ruimtebedrijven hebben wij afgesproken om een gedeelte van ons geld in een pot te doen. Met dit geld kunnen wij met zijn allen het afval opruimen en de ruimte schoonhouden!</p>
+            <button className="quizz-btn exit-btn btn" onClick={exit}> Ga verder</button>
+        </div>
         <Modal className="modal-rebus" isOpen={modalIsOpen}>
             <button onClick={setModalIsOpenToFalse}>x</button>
             <ModalRebus/>
