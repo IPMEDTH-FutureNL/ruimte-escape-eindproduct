29ea413eda1a724e2e106fbc38910893b1df23d9
daniquemont <danique.montanus@gmail.com>
2022-05-31 16:13:43 +0200

    spelling aanpassing

--- src/components/quizzPuzzle/QuizzPuzzle.js
+++ src/components/quizzPuzzle/QuizzPuzzle.js
@@ -55,7 +55,7 @@ const QuizzPuzzle = () => {
             <img src={Zonnestelsel} alt="" onClick={setZModalIsOpenToTrue} className="quiz-zonnestelsel"/>
             <img src={Computer} alt="" onClick={setCModalIsOpenToTrue} className="quiz-computer" />
             <div className="quiz-intro" id="open">
-                <p className="text-white">Je bent in de computerkamer! Ons taak is om straks de vuilnis in de ruimte op te ruimen. Het is niet alleens ons taak om naar mars te gaan, maar ook om de ruimte schoon te houden! Om straks de vuilnis op te kunnen ruimen heb je een code nodig die ergens op de computer te vinden is in de kamer. Kunnen jullie op zoek gaan naar deze code?</p>
+                <p className="text-white">Je bent in de computerkamer! Onze taak is om straks het vuilnis in de ruimte op te ruimen. Het is niet alleens onze taak om naar mars te gaan, maar ook om de ruimte schoon te houden! Om straks het vuilnis op te kunnen ruimen heb je een code nodig die ergens op de computer te vinden is in de kamer. Kunnen jullie op zoek gaan naar deze code?</p>
                 <button className="quizz-btn exit-btn btn" onClick={exit}> Ga verder</button>
             </div>
             
